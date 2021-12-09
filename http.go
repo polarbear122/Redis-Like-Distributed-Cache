e530f1ef04a217add614d98ecf1d7b56af041e35
polarbear122 <53006681+polarbear122@users.noreply.github.com>
2021-12-09 13:03:40 +0800

    Add files via upload

---
+++ http.go
@@ -0,0 +1,206 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"main/consistent"
+	"net/http"
+	"net/url"
+	"strings"
+	"sync"
+)
+
+const(
+	defaultBasePath = "/_main"
+	/*
+		节点数，后续代码添加
+	*/
+	defaultReplicas = 50
+)
+
+
+//线程池
+/*
+	self 用来记录自己的地址，包括主机名/IP和端口
+	basePath 作为节点间通讯地址的前缀，默认是/_main/
+	以http://XXX.com/_main/开头的请求，就用于节点间的访问
+
+*/
+type HTTPPool struct {
+	self 	 string
+	basePath string
+	/*
+		后续代码添加
+	*/
+	/*
+		监视节点和httpGetters
+	*/
+	mu 		    sync.Mutex
+	/*
+		一致性哈希算法的Map，见readMe中的一致性哈希章节的示意图
+		用来根据具体的key选择节点
+	*/
+	peers       *consistent.Map
+	/*
+		key值为：http://xxxxx
+		每个远程连接节点对应一个httpGetter
+		因为httpGetter与远程节点的地址 baseURL有关
+	*/
+	httpGetters map[string]*httpGetter
+}
+
+func NewHTTPPool(self string) *HTTPPool  {
+	return &HTTPPool{
+		self:	self,
+		basePath:	defaultBasePath,
+	}
+}
+
+// Core Code
+
+/*
+ServeHTTP 的实现逻辑是比较简单的，首先判断访问路径的前缀是否是 basePath，不是返回错误。
+我们约定访问路径格式为 /<basepath>/<groupname>/<key>，
+通过 groupname 得到 group 实例，再使用 group.Get(key) 获取缓存数据。
+最终使用 w.Write() 将缓存值作为 httpResponse 的 body 返回。
+*/
+func (p *HTTPPool) Log(format string,v ...interface{}){
+	log.Printf("[Server %s] %s",p.self,fmt.Sprintf(format,v...))
+}
+
+func (p *HTTPPool) ServeHTTP (w http.ResponseWriter,r *http.Request){
+	if !strings.HasPrefix(r.URL.Path,p.basePath){
+		panic("HTTPPool serving unexpected path:"+r.URL.Path)
+	}
+	p.Log("%s %s",r.Method,r.URL.Path)
+	parts:=strings.SplitN(r.URL.Path[len(p.basePath):],"/",2)
+	if len(parts)!= 2{
+		http.Error(w,"bad request",http.StatusBadRequest)
+		return
+	}
+
+	groupName := parts[0]
+	key:=parts[1]
+
+	group := GetGroup(groupName)
+
+	if group == nil{
+		http.Error(w,"no such group:" + groupName,http.StatusNotFound)
+		return
+	}
+
+	view,err := group.Get(key)
+	if err!=nil {
+		http.Error(w,err.Error(),http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/octet-stream")
+	w.Write(view.ByteSlice())
+}
+
+/*
+	httpGetter 实现 peer.go中的peerGetter接口
+	baseURL 表示将要访问的远程节点的地址
+	例如http://example.com/main/
+	使用http.Get()方式获取返回值，并转换为[]bytes类型
+*/
+type httpGetter struct{
+	baseURL string
+}
+
+func (hg *httpGetter)Get(group string,key string)([]byte,error){
+	var b  strings.Builder
+	b.WriteString(hg.baseURL)
+	/*
+		url.QueryEscape用于转义url字段
+		可以被安全地防止
+	*/
+	b.WriteString(url.QueryEscape(group))
+	b.WriteString(url.QueryEscape(key))
+	s := b.String()
+
+	res,err := http.Get(s)
+	if err != nil {
+		return nil,err
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode!= http.StatusOK {
+		return nil,fmt.Errorf("server returned : %v",res.Status)
+	}
+
+	/*
+		bytes是字节类型的数组
+		具体设计见cache.go和lru.go
+	*/
+	bytes,err := ioutil.ReadAll(res.Body)
+
+	/*
+		异常
+	*/
+	if err != nil{
+		return nil, fmt.Errorf("reading response body: %v",err)
+	}
+
+	return bytes,nil
+}
+
+var _ PeerGetter = (*httpGetter)(nil)
+
+
+/*
+	实现PeerPicker接口
+*/
+
+/*
+	Set()实例化了一致性哈希算法
+	并添加了传入的节点
+*/
+func (p *HTTPPool)Set(peers ...string){
+	p.mu.Lock()
+	defer p.mu.Unlock()
+	/*
+		实例化
+	*/
+	p.peers = consistent.New(defaultReplicas,nil)
+	/*
+		添加传入节点
+	*/
+	p.peers.Add(peers...)
+	/*
+		为每个节点配置Getter
+	*/
+	p.httpGetters = make(map[string]*httpGetter,len(peers))
+
+	/*
+		peer如http://xxxx.com/
+		basePath如 _main/
+		合起来就是一个节点的访问路径
+	*/
+	for _,peer := range peers{
+		p.httpGetters[peer] = &httpGetter{
+			baseURL:peer+p.basePath,
+		}
+
+	}
+}
+/*
+	PickerPeer() 包装了一致性哈希算法的 Get() 方法，
+	根据具体的 key，选择节点，返回节点对应的 HTTP 客户端。
+	注意，这只是返回一个节点，而不是返回缓存的value
+	返回缓存的实现在main.go的load()中
+*/
+func (p *HTTPPool) PickPeer(key string) (PeerGetter, bool) {
+	p.mu.Lock()
+	defer p.mu.Unlock()
+	if peer := p.peers.Get(key); peer != "" && peer != p.self {
+		p.Log("Pick peer %s", peer)
+		return p.httpGetters[peer], true
+	}
+	return nil, false
+}
+
+var _ PeerPicker = (*HTTPPool)(nil)
+
---
+++ main.go
@@ -0,0 +1,159 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"sync"
+)
+
+/*
+负责与外部交互
+控制缓存存储和获取的主流程
+*/
+
+//回调函数
+//当缓存不存在，从其他数据源获取，并添加到缓存
+type Getter interface {
+	Get(key string)([]byte,error)
+}
+
+/*
+定义一个函数类型 F，并且实现接口 A 的方法，
+然后在这个方法中调用自己。
+这是 Go 语言中将其他函数转换为接口 A 的常用技巧
+（参数返回值定义与 F 一致）
+*/
+
+type GetterFunc func(key string)([]byte,error)
+
+func (f GetterFunc) Get(key string)([]byte,error){
+	return f(key)
+}
+
+/*
+Groups
+缓存的集合
+Groups =Group的集合= n个缓存表
+注意区分Groups和Group
+*/
+
+type Group struct {
+	name string
+	getter Getter
+	mainCache cache
+	/*
+		后续添加
+	*/
+	/*
+		一致性哈希算法的节点表
+	*/
+	peers PeerPicker
+}
+
+/*
+	注册节点
+*/
+func(g *Group) RegisterPeers(peers PeerPicker){
+	if g.peers != nil{
+		panic("注册器已被调用过")
+	}
+	g.peers = peers
+}
+
+/*
+	load方法
+	如果是远程节点，使用PickPeer()方法选择节点
+	本地节点则使用getLocally()
+*/
+func (g *Group) load(key string) (value ByteView, err error) {
+	if g.peers != nil {
+		//选择节点
+		if peer, ok := g.peers.PickPeer(key); ok {
+			//远程节点-取值
+			if value, err = g.getFromPeer(peer, key); err == nil {
+				return value, nil
+			}
+			log.Println("[main] Failed to get from peer", err)
+		}
+	}
+
+	return g.getLocally(key)
+}
+
+/*
+	远程节点取值实现
+*/
+func (g *Group) getFromPeer(peer PeerGetter, key string) (ByteView, error) {
+	/*
+		这里的peer类型其实是http.go中的httpGetter
+	*/
+	bytes, err := peer.Get(g.name, key)
+	if err != nil {
+		return ByteView{}, err
+	}
+	return ByteView{b: bytes}, nil
+}
+
+
+func (g *Group) getLocally(key string)(ByteView,error) {
+	bytes,err := g.getter.Get(key)
+	if err!=nil{
+		return ByteView{},err
+	}
+	value := ByteView{b:cloneBytes(bytes)}
+	//将远程数据添加到当前缓存
+	g.popularCache(key,value)
+	return value,nil
+}
+func (g *Group) popularCache(key string,value ByteView) {
+	g.mainCache.add(key,value)
+}
+
+var (
+	mu  sync.RWMutex
+	groups = make(map[string]*Group)
+)
+
+func NewGroup(name string,cacheBytes int64, getter Getter) *Group{
+	//getter是一个函数类型的参数
+	if getter == nil{
+		panic("nil Getter")
+	}
+
+	mu.Lock()
+	defer mu.Unlock()
+	g:= &Group{
+		name: name,
+		getter: getter,
+		mainCache:cache{cacheBytes:cacheBytes},
+	}
+	groups[name] = g
+	return g
+}
+
+//返回相应的group
+func GetGroup(name string) *Group{
+	mu.RLock()
+	g:=groups[name]
+	mu.RUnlock()
+	return g
+}
+
+//get
+func (g *Group)Get(key string) (ByteView,error){
+	if key==""{
+		return ByteView{},fmt.Errorf("key is required")
+	}
+
+	if v,ok:=g.mainCache.get(key);ok{
+		log.Println("[main] hit")
+		return v,nil;
+	}
+	//从其他数据源获取
+	return g.load(key)
+}
+
+
+
+
+
